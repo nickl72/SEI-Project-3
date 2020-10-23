9edb66723943e8dfd57db2f705bfdeb8fd8564c0
Chris Myers <myerschristopher@johndeere.com>
2020-10-23 17:01:16 -0500

    form fields added

--- brewery-app/src/components/SearchForm.js
+++ brewery-app/src/components/SearchForm.js
@@ -7,10 +7,23 @@ const SearchForm = () => {
 
     return (
         <form>
-            <h2>Search for a Brewery</h2>
-            <input type='text' placeholder='Search by city' />
-            <input type='text' placeholder='Search by city' />
-            <input type='text' placeholder='Search by city' />
+            <h2>Search for a Brewery by...</h2>
+            <input type='text' placeholder='City' />
+            <select >
+                <option selected disabled hidden>State</option>
+                <option>Iowa</option>
+            </select>
+            <input type='text' placeholder='Zip code' />
+            <input type='text' placeholder='Brewery name' />
+            <select >
+                <option selected disabled hidden>Brewery type</option>
+                {/* todo, breweries must be of types:
+                    micro, regional, brewpub, large, planning, bar, contract, proprietor
+                */}
+            </select>
+            {/* todo per_page, sort by one or more fields */}
+
+            <input type='submit' value='Search' />
         </form>
     )
 
