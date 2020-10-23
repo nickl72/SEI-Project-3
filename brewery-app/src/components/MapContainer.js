67f0ab2d121adde26c13e575eecd4c45c18212ac
Chris Myers <myerschristopher@johndeere.com>
2020-10-23 11:33:08 -0500

    ignore .env

--- brewery-app/src/components/MapContainer.js
+++ brewery-app/src/components/MapContainer.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
- 
-export class MapContainer extends Component {
-}
+import GoogleMapReact from 'google-map-react';
+import Styled from 'styled-components';
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyB4af27NRq5CVPXSz-lPlNKr4rf6DAC2xY')
-})(MapContainer)
+
+
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+  
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact style={{ height: '90vh', width: '90%' }}
+          bootstrapURLKeys={{ key: 'AIzaSyB4af27NRq5CVPXSz-lPlNKr4rf6DAC2xY' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
