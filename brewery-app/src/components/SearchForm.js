79acf34043782ed5a65b51f5d00ce57cf7d6bd8a
Chris Myers <myerschristopher@johndeere.com>
2020-10-23 17:48:58 -0500

    hook state in search form

--- brewery-app/src/components/SearchForm.js
+++ brewery-app/src/components/SearchForm.js
@@ -2,28 +2,61 @@ import React, { useState } from 'react';
 
 const SearchForm = () => {
     const [searchData, setSearchData] = useState({
-        
+        // todo import states names to not need 50 lines
+        city: '',
+        state: '',
+        zipCode: '',
+        breweryName: '',
+        breweryType: ''
     })
+
+    const handleInputChange = (e) => {
+        searchData[e.target.name] = e.target.value
+    }
 
     return (
         <form>
             <h2>Search for a Brewery by...</h2>
-            <input type='text' placeholder='City' />
-            <select >
+            <input 
+                type='text' 
+                name='city'
+                placeholder='City'
+                onChange={handleInputChange}
+            />
+            <select 
+                name='state'
+                onChange={handleInputChange}
+            > 
                 <option selected disabled hidden>State</option>
                 <option>Iowa</option>
             </select>
-            <input type='text' placeholder='Zip code' />
-            <input type='text' placeholder='Brewery name' />
-            <select >
-                <option selected disabled hidden>Brewery type</option>
+            <input 
+                type='text' 
+                name='zipCode'
+                placeholder='Zip code'
+                onChange={handleInputChange}
+            />
+            <input 
+                type='text' 
+                name='breweryName'
+                placeholder='Brewery name'
+                onChange={handleInputChange}
+            />
+            <select 
+                name='breweryType'
+                onChange={handleInputChange}
+            > 
+                <option selected disabled>Brewery type</option>
                 {/* todo, breweries must be of types:
                     micro, regional, brewpub, large, planning, bar, contract, proprietor
                 */}
             </select>
             {/* todo per_page, sort by one or more fields */}
 
-            <input type='submit' value='Search' />
+            <input 
+                type='submit' 
+                value='Search'
+            />
         </form>
     )
 
