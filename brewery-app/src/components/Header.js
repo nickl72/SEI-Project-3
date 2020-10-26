6b7edca126af3f7907e9a905d5f131488287a48e
Chris Myers <myerschristopher@johndeere.com>
2020-10-26 13:44:14 -0500

    header WIP

---
+++ brewery-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchForm from './SearchForm';
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Puzzles</h1>
+            {/* <SearchForm sendResults={setSearchData} /> */}
+        </div>
+    )
+}
+
+export default Header;
