9842c8f4c636ce207d385986fc8708e3b5d79760
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-10-28 15:01:48 -0400

    Formatted Email message

--- brewery-app/src/components/EmailForm.js
+++ brewery-app/src/components/EmailForm.js
@@ -3,46 +3,54 @@ import { useSelector } from 'react-redux';
 import { barCrawl } from '../features/barCrawlSlice';
 import sendEmail from '../app/email';
 
-// {address_2: null
-// address_3: null
-// brewery_type: "micro"
-// city: "West Des Moines"
-// country: "United States"
-// county_province: null
-// created_at: "2018-07-24T01:33:24.495Z"
-// id: 2817
-// latitude: "41.5936597"
-// longitude: "-93.7489517002412"
-// name: "Twisted Vine Brewery"
-// phone: "5157202940"
-// postal_code: "50266-1111"
-// state: "Iowa"
-// street: "3320 Westown Pkwy"
-// updated_at: "2018-08-24T00:38:17.525Z"
-// website_url: "http://www.twistedvinebrewery.com"
-// __proto__: Object
-// length: 1
-// __proto__: Array(0)}
-
 
 const EmailForm = () => {
     const barCrawlList = useSelector(barCrawl);
 
+    function formatPhoneNumber(phoneNumber) {
+        let match = phoneNumber.match(/^(\d{3})(\d{3})(\d{4})$/)
+        if (match) {
+            return `(${match[1]}) ${match[2]}-${match[3]}`
+        }
+    }
     const sendList = (e) => {
         e.preventDefault();
         console.log('list');
-        let htmlMessage = `<div>`
+        let htmlMessage = `
+            <div style="
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; 
+                display: flex; 
+                flex-direction: column; 
+                align-items: center;"
+            >
+        `
         barCrawlList.forEach(bar => {
             htmlMessage = htmlMessage + `
-                <br/>
-                <br/>
-                <h2><a href=${bar.website_url} >${bar.name}</a></h2>
-                <p>${bar.street}<br/>
-                   ${bar.city}, ${bar.state} 
-                </p>
-                <br/>
-                <p>${bar.phone}</p>
-                `
+                <div style='
+                    background: #f9efc1; 
+                    padding: 1em; 
+                    margin: 1em;
+                    border-radius: 2em; 
+                    color: #331a04;
+                    width: 15em;'
+                >
+                    <h2><a href=${bar.website_url} target='_blank' style='color: #331a04;'>${bar.name}</a></h2>
+                    <address>
+                        <a 
+                            href='https://www.google.com/maps/place/${bar.street},${bar.city},${bar.state}' 
+                            target='_blank'
+                            style='
+                                color: #331a04;
+                                text-decoration: none;
+                            '
+                        >
+                            ${bar.street}<br/>
+                            ${bar.city}, ${bar.state}
+                        </a> 
+                    </address>
+                    <p>Call: <a href='tel:${bar.phone}' >${formatPhoneNumber(bar.phone)}</a></p>
+                </div>
+            `
         })
         htmlMessage = htmlMessage + `</div>`
         sendEmail(e.target.name.value, htmlMessage, e.target.email.value)
