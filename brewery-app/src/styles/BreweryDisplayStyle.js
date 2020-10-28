5ad52d0a51260ed97f57ee6236f0143280119ad9
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-10-27 22:52:12 -0500

    functioning legend, shows only icons that are present on page

--- brewery-app/src/styles/BreweryDisplayStyle.js
+++ brewery-app/src/styles/BreweryDisplayStyle.js
@@ -1,9 +1,10 @@
 import styled, { keyframes } from "styled-components";
 import {rotateOutUpRight, rotateInDownRight, bounceIn, merge} from "react-animations";
-import rotateIn from "react-animations/lib/rotate-in";
 
-const drinkIn = merge(rotateOutUpRight, rotateInDownRight);
 const bounceInAnimation = keyframes`${bounceIn}`;
+const drinkOut =  keyframes`${rotateOutUpRight}`;
+const drinkIn = keyframes`${rotateInDownRight}`;
+const merger = merge(bounceIn, rotateOutUpRight);
 
 export const ShowPageContainer = styled.div`
     display: flex;
@@ -39,16 +40,21 @@ export const Holder = styled.div`
         justify-content: space-between;
     }
 `
-export const Icon = styled.img`
+export const BasicIcon = styled.img`
     height: 22px;
     width: 22px;
+    margin: 5px;
+`
+
+export const Icon = styled(BasicIcon)`
+    margin: 0;
     position: relative;
     cursor: pointer;
     border-radius: 5px;
     background: radial-gradient(darkgray 15%, transparent);
-    animation: 1s ${bounceInAnimation};
+    animation: 2s ${bounceInAnimation};
     &.light {
-        animation: 1s ${drinkIn};
+        animation: 1s ${merger};
         box-shadow: 0px 0px .5vmin .5vmin navy;
         background: navy;
         
@@ -72,4 +78,27 @@ export const Modal = styled.div`
     padding: 5px;
     width: 150px;
     z-index: 1;
+`
+
+export const LegendHolder = styled.div`
+    background: white;
+    margin: 10px;
+    padding: 8px;
+    border-radius: 2px;
+    box-shadow: 0px 0px 8px gray;
+    min-width: 50px;
+    z-index: 2;
+    min-height: 12px;
+    position: absolute;
+
+    .legendRow {
+        display: flex;
+        align-items: center;
+    }
+    
+`
+export const Title = styled.div`
+    font-weight: bold;
+    text-decoration: underline;
+    text-align: center;
 `
