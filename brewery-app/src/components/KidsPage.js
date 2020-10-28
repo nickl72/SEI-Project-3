a02c3b852a8511193bbbdfed1f4fbc2f96969ce8
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-10-28 14:43:49 -0500

    added message to kids page

--- brewery-app/src/components/KidsPage.js
+++ brewery-app/src/components/KidsPage.js
@@ -6,9 +6,10 @@ const fadeInAnimation = keyframes`${fadeIn}`;
 
 
 const Div = styled.div`
-    height: 100vh;
+    height: 80vh;
     width: 100vw;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     animation: ${fadeInAnimation} 2s 1;
@@ -17,6 +18,7 @@ const Div = styled.div`
 const KidsPage = () => {
     return (
         <Div>
+            <h1>Sorry, Bud, the bouncer caught you.</h1> <h1> You have to be 21 to enter!</h1>
             <img src='https://media1.tenor.com/images/16ccbfc83e392ca0c393084764533764/tenor.gif' alt='Kid spills drink gif'/>
         </Div>
     )
