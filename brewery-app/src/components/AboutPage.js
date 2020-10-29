2249569243f26c4bd583181b576406df0371d180
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-10-29 14:39:15 -0400

    Added Description to about page

--- brewery-app/src/components/AboutPage.js
+++ brewery-app/src/components/AboutPage.js
@@ -5,6 +5,7 @@ const AboutPage = () => {
     return (
         <div>
             <h1>About Puzzles</h1>
+            <p>Puzzles is the Bar Crawl tool you've been Waiting for! Whether it's in a town you know, or one you're visiting, we can help you find the best bars, breweries, and brewpubs to explore. For those of you that want to try them all, we've made it easy to pick out and plan a bar crawl adventure for you and your friends. Once you've made the plan we'll send you an email with your itinerary so no one gets lost in the journey.</p>
             <p>Puzzles is your one stop shop to find breweries in your area, or anywhere you plan to travel.</p>
             <ul>
                 <li> MicroBrewery</li>
@@ -15,7 +16,6 @@ const AboutPage = () => {
                 <li> Contract</li>
                 <li> Proprieter</li>
             </ul>
-            <Link to='/'>Return to Search Page</Link>
         </div>
     )
 }
