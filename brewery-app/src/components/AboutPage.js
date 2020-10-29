534c2462bb77b0428a8fbdc48b53f498d40c6fa9
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-10-29 15:48:11 -0400

    Added styling to About page

--- brewery-app/src/components/AboutPage.js
+++ brewery-app/src/components/AboutPage.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Div = styled.div`
+    width: 100%;
+    margin: 0;
+    text-align: center;
+    font-size: xx-large;
+    color: #331a04;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    p {
+        width: 80%;
+        font-weight: 500;
+    }
+`
 const AboutPage = () => {
     return (
-        <div>
+        <Div>
             <h1>About Puzzles</h1>
-            <p>Puzzles is the Bar Crawl tool you've been Waiting for! Whether it's in a town you know, or one you're visiting, we can help you find the best bars, breweries, and brewpubs to explore. For those of you that want to try them all, we've made it easy to pick out and plan a bar crawl adventure for you and your friends. Once you've made the plan we'll send you an email with your itinerary so no one gets lost in the journey.</p>
-            <p>Puzzles is your one stop shop to find breweries in your area, or anywhere you plan to travel.</p>
-            <ul>
-                <li> MicroBrewery</li>
-                <li> Regional Brewery</li>
-                <li> Brewpub</li>
-                <li> Large</li>
-                <li> Bar</li>
-                <li> Contract</li>
-                <li> Proprieter</li>
-            </ul>
-        </div>
+            <p>Puzzles is the Bar Crawl tool you've been Waiting for!</p>
+            <p>Whether it's a town you know, or one you've never been to, 
+                we can help you find the best bars, breweries, and brewpubs to explore. 
+                For those of you that want to try them all, we've made it easy to pick out 
+                and plan a bar crawl adventure for you and your friends. Once you've made the 
+                plan we'll send you an email with your itinerary so no one gets lost in the 
+                journey.</p>
+        </Div>
     )
 }
 
